a919b1881cc2733de788cfd8ba9ae17a986e5b91
rlatn <rlatn@1.234.192.237>
2021-01-23 00:32:29 +0900

    2021-01-23 12:31 분 mini - 1.1 DB수정

--- src/mini1/Application.java
+++ src/mini1/Application.java
@@ -6,36 +6,42 @@ public class Application {
 
 	public static void main(String[] args) {
 //
-//		Scanner sc = new Scanner(System.in);
-//		
-//		while(true) {
-//			System.out.println("======회원관리 프로그램======");
-//			System.out.println("1.로그인");
-//			System.out.println("2.회원 가입");
-//			System.out.println("3.회원 목록 확인");
-//			System.out.println("3.예약확인");
-//			System.out.println("4.개인정보확인");
-//			System.out.println("9.프로그램 종료");
-//			System.out.print("메뉴 선택 :");
-//
-//			int no = sc.nextInt();
-//			sc.nextLine();
-//			
-//			switch(no) {
-//			case 1 : break;
-//			case 2 : break;
-//			case 3 : break;
-//			case 9 : return ;
-//			default: 
-//			}
-//		}
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			System.out.println("======회원관리 프로그램======");
+			System.out.println("1.로그인");
+			System.out.println("2.회원 가입");
+			System.out.println("3.회원 목록 확인");
+			System.out.println("3.예약확인");
+			System.out.println("4.개인정보확인");
+			System.out.println("9.프로그램 종료");
+			System.out.print("메뉴 선택 :");
+
+			int no = sc.nextInt();
+			sc.nextLine();
+			
+			switch(no) {
+			case 1 : break;
+			case 2 : break;
+			case 3 : break;
+			case 9 : return ;
+			default: 
+			}
+		}
 		
 		
 		
 //		MemberDB memberDB = new MemberDB();	
-//		memberDB.saveDBMethod();
 //		memberDB.loadingDBMethod();
-//		System.out.println(memberDB.loadingDBMethod().toString());
+//		memberDB.menberUpdate();
+//		System.out.println(MemberDB.getListDB());
+//		MemberDB.listDB.get(0).setName("난광복");
+//		System.out.println(MemberDB.getListDB());
+//		memberDB.informationChang();
+//		System.out.println(MemberDB.getListDB());
+		
+		
 
 	}
 
--- src/mini1/MemberDB.java
+++ src/mini1/MemberDB.java
@@ -8,15 +8,34 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.List;
+
+import test1.DB;
+import test1.userDTO;
 
 
 
 public class MemberDB {
 	
+	public static List<Member> listDB = new ArrayList<Member>();
 	
-	public void saveDBMethod() {
+	
+	public static List<Member> getListDB() {
+		return listDB;
+	}
+
+	public static void setListDB(List<Member> listDB) {
+		MemberDB.listDB = listDB;
+	}
+//	Member user
+	public void menberUpdate() {
 		
-		Member user = new Member("rlatngud", "rlatngud", "김수형", "강남구", 20, '남', "2021-01-21");
+		Member user = new Member("bbb", "bbb", "박광복", "강남구", 28, '남', "2021-01-25");
+		
+		
+		
+		
+//		Member[] array = listDB.toArray(new Member[listDB.size()]);
 		
 		 FileOutputStream fos = null;
 	     ObjectOutputStream oos = null;
@@ -57,8 +76,66 @@ public class MemberDB {
 			
 	}
 	
+	
+	public void informationChang() {
+		
+
+		
+		Member[] memberArray = listDB.toArray(new Member[listDB.size()]);
+		
+		for(int i = 0; i < memberArray.length; i++){
+            System.out.println(memberArray[i]);
+         }
+		
+		 FileOutputStream fos = null;
+	     ObjectOutputStream oos = null;
+	     
+	     try {
+	         
+	         if(new File("C:/miniDB/miniDB.txt").exists()) // 저장할 파일 위치 임닌다 마지막 텍스트 
+	         {
+	          
+	          fos = new FileOutputStream("C:/miniDB/miniDB.txt");    
+	          oos = new ObjectOutputStream(fos);
+	         }else
+	         {
+	        
+	          fos = new FileOutputStream("C:/miniDB/miniDB.txt");    
+	          oos = new ObjectOutputStream(fos);
+	         }
+	      
+	         for(int i = 0; i < memberArray.length; i++){
+		            oos.writeObject(memberArray[i]);
+		         }
+	         
+	         
+	         oos.flush();
+	         
+
+	      } catch (Exception e) {
+	         e.printStackTrace();
+	      }finally{	// 때려죽어도 무조건 실행됨
+				try {
+				if(oos != null) {
+					oos.close();
+					} 
+				if(fos != null) {
+					fos.close();
+					} 
+				}catch (IOException e) {
+					e.printStackTrace();
+				}
+		
+			}
+			
+	}
+	
+	
+	
+	
+	
 //	ArrayList<Member>
-	public ArrayList<Member> loadingDBMethod() {
+	public void loadingDBMethod() {
 		
 		ArrayList<Member> list = new ArrayList<Member>();
 		
@@ -97,8 +174,8 @@ public class MemberDB {
 			}
 	
 		}
-		
-		return list;
+		MemberDB.setListDB(list);
+//		return list;
 	}
 	
 	
